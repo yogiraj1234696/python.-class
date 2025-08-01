2a91dd7114016c1ab5be83c19a67f8c5a2d8a4ed
yogiraj1234696 <umareyogi@gmail.com>
2025-08-01 09:01:23 +0530

    First Program

---
+++ demo.py
@@ -0,0 +1,3 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+print(a+b)                                                                                                                                                                                                                                                                                       
